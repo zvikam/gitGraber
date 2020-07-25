db67c9817cea90be261912a4bfb417f94c571b78
Zvika Meiseles <zvika.meiseles@gmail.com>
2020-07-25 17:24:10 +0300

    new fileGraber code cleanup

--- fileGraber.py
+++ fileGraber.py
@@ -2,24 +2,21 @@
 
 import requests
 import re
-import json
-import time
 import argparse
-import mmap
 import argcomplete
-import config
 import glob
-import tokens
 import os
-import time
-import urllib.parse
 from functools import partial
-from datetime import datetime
-from pprint import pprint
 from termcolor import colored
-from urllib.parse import urlparse
 from multiprocessing.dummy import Pool
-from crontab import CronTab
+
+import config
+import tokens
+
+
+def log(verbose, *args):
+    if verbose:
+        print(*args)
 
 
 def createEmptyBinaryFile(name):
@@ -76,35 +73,27 @@ def checkToken(content, tokensMap, tokensCombo):
 
 def notifySlack(message):
     if not config.SLACK_WEBHOOKURL:
-        print('Please define Slack Webhook URL to enable notifications')
+        log(True, 'Please define Slack Webhook URL to enable notifications')
         exit()
     requests.post(config.SLACK_WEBHOOKURL, json={'text': ':new:'+message})
 
 
 def notifyTelegram(message):
     if not config.TELEGRAM_CONFIG or not config.TELEGRAM_CONFIG.get("token") or not config.TELEGRAM_CONFIG.get("chat_id"):
-        print('Please define Telegram config to enable notifications')
+        log(True, 'Please define Telegram config to enable notifications')
         exit()
 
     telegramUrl = "https://api.telegram.org/bot{}/sendMessage".format(config.TELEGRAM_CONFIG.get("token"))
     requests.post(telegramUrl, json={'text': message, 'chat_id': config.TELEGRAM_CONFIG.get("chat_id")})
 
 
-def writeToWordlist(content, wordlist):
-    f = open(wordlist, 'a+')
-    s = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
-    filename = content.split('/')[-1]
-    if s.find(bytes(filename,'utf-8')) == -1:
-        f.write(filename + '\n')
-
-
 def displayResults(result, tokenResult, filename):
     possibleTokenString = '[!] POSSIBLE ' + tokenResult[result] + ' TOKEN FOUND'
-    print(colored(possibleTokenString,'green'))
+    log(True, colored(possibleTokenString,'green'))
     pathString = '[+] FILE : ' + filename
-    print(pathString)
+    log(True, pathString)
     tokenString = '[+] Token : ' + result 
-    print(tokenString.strip())
+    log(True, tokenString.strip())
     return possibleTokenString+'\n'+pathString+'\n'+tokenString
 
 
@@ -113,11 +102,9 @@ def doSearchFilesystem(args, tokenMap, tokenCombos, file):
         with open(file, 'r') as f:
             content = f.read()
     except:
-        if args.verbose:
-            print("[+] skipping binary file", file)
+        log(args.verbose, "[+] skipping binary file", file)
         return
-    if args.verbose:
-        print("[+] checking", file)
+    log(args.verbose, "[+] checking", file)
     tokensResult = checkToken(content, tokenMap, tokenCombos)
     for token in tokensResult.keys():
         displayMessage = displayResults(token, tokensResult, file)
@@ -125,8 +112,6 @@ def doSearchFilesystem(args, tokenMap, tokenCombos, file):
             notifySlack(displayMessage)
         if args.telegram:
             notifyTelegram(displayMessage)
-        if args.wordlist:
-            writeToWordlist(file, args.wordlist)
 
 
 def findFiles(mask):
@@ -138,8 +123,7 @@ def findFiles(mask):
 def searchFilesystem(args):
     tokenMap, tokenCombos = tokens.initTokensMap()
 
-    if args.verbose:
-        print("[+] scanning", args.mask)
+    log(args.verbose, "[+] scanning", args.mask)
 
     pool = Pool(int(args.max_threads))
     pool.map(partial(doSearchFilesystem, args, tokenMap, tokenCombos), findFiles(args.mask))
@@ -150,27 +134,16 @@ def searchFilesystem(args):
 parser = argparse.ArgumentParser()
 argcomplete.autocomplete(parser)
 parser.add_argument('-t', '--threads', action='store', dest='max_threads', help='Max threads to speed the file handling', default="3")
-parser.add_argument('-m', '--mask', action='store', dest='mask', help='Specify file mask (-m "*.java")')
+parser.add_argument('-m', '--mask', action='store', dest='mask', help='Specify file mask (-m "*.java")', default='./**/*')
 parser.add_argument('-v', '--verbose', action='store_true', help='Enable verbose output', default=False)
 parser.add_argument('-s', '--slack', action='store_true', help='Enable slack notifications', default=False)
 parser.add_argument('-tg', '--telegram', action='store_true', help='Enable telegram notifications', default=False)
-parser.add_argument('-w', '--wordlist', action='store', dest='wordlist', help='Create a wordlist that fills dynamically with discovered filenames')
 args = parser.parse_args()
 
 if not args.mask or args.mask == "":
-    print('No mask (-m or --mask) is specified, searching all files in the current directory')
-    args.mask = './**/*'
-    fileMask = args.mask
+    log(args.verbose, 'No mask (-m or --mask) is specified, searching all files in the current directory')
 
-fileMask = args.mask
 path_script = os.path.dirname(os.path.realpath(__file__))
 
-# If wordlist, check if file is binary initialized for mmap
-if args.wordlist:
-    initFile(args.wordlist)
-
-# Init URL file
-initFile(config.GITHUB_URL_FILE)
-
 # Search filesystem
-responses = searchFilesystem(args)
+searchFilesystem(args)
